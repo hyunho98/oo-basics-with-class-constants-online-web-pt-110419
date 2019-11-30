282e782596b20c3eacf6c8ea7258d05340faf06e
Alex Lee <hyunho98@gmail.com>
2019-11-30 00:54:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,10 +2,14 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+
+    if BRANDS.includes(brand) == false
+      BRANDS << brand
+    end
   end
 
   def cobble
